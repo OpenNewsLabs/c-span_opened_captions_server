c7a9a75081209ee5588f73f91883b17750991013
Ryan Mark <ryan@mrk.cc>
2017-01-18 09:52:49 -0500

    refactor for always on, setup for heroku

--- index.js
+++ index.js
@@ -9,22 +9,67 @@ const fs = require('fs')
 const http = require('http')
 const url = require('url')
 
-const fpath = 'transcription.txt'
+const transcriptFile = 'transcription.txt'
+const ttl = 20 * 60 * 1000 // 20 mins -> microseconds
+const cacheCheckInterval = 5 * 60 * 1000 // 5 mins -> microseconds
+var cache = []
+if ( fs.existsSync(transcriptFile) ) {
+  cache.push({t: Date.now(), r: fs.readFileSync(transcriptFile)})
+}
+setInterval(cleanCache, cacheCheckInterval)
 
-var str = fs.readFileSync(fpath, 'utf8')
-const stream = fs.createWriteStream(fpath, {flags: 'a'})
-
-
+const txt = fs.createWriteStream(transcriptFile)
 const socket = io.connect('https://openedcaptions.com:443')
-socket.on('word', data => {
-  //add additional formating such as downcase and a line breaks for occurences of`>>`
-  const word = ' ' + data.data.body.toLowerCase().replace(/>>/g, "\n\n")
-  stream.write(word)
-  str += word      
+socket.on('content', data => {
+  txt.write(data.data.body)
+  if ( data.data.body === "\r\n" ) { return; }
+  const dat = {t: Date.now(), r: data.data.body}
+  console.log(dat.t, dat.r)
+  cache.push(dat)
 })
 
 http.createServer((req, res) => {
+  const now = Date.now()
   const query = url.parse(req.url, true).query
-  const offset = query.offset || 0
-  res.end(str.substring(offset))
-}).listen(5000)
+  const timestamp = parseInt(query.since || 0)
+  res.setHeader('Content-Type', 'application/json')
+  res.end(JSON.stringify({
+    now: now,
+    captions: formatText(getWordsSince(timestamp))
+  }))
+}).listen(process.env.PORT || 5000)
+
+function formatText(str) {
+  return str.toLowerCase()
+    .replace("\r\n", ' ')
+    .replace(/\s+/g, ' ')
+    .replace(/\s+(!|\?|;|:|,|\.)/g, '$1')
+    .replace(/ mrs\.? /gi, ' Mrs. ')
+    .replace(/ ms\.? /gi, ' Ms. ')
+    .replace(/ mr\.? /gi, ' Mr. ')
+    .replace(/ dr\.? /gi, ' Dr. ')
+    .replace(/ i /g, ' I ')
+    .replace(/ senator (\w)/gi, (match, a) => { return ' Senator ' + a.toUpperCase() })
+    .replace(/(!|\?|:|\.|>>)\s+(\w)/g, (match, a, b) => { return a + ' ' + b.toUpperCase() })
+    .replace(/\s*>>\s*/g, "\n\n")
+    .trim()
+}
+
+function getWordsSince(timestamp) {
+  var ret = []
+  cache.forEach((val, i) => {
+    if ( val.t >= parseInt(timestamp) ) {
+      ret.push(val.r)
+    }
+  })
+  return ret.join(' ')
+}
+
+function cleanCache() {
+  const ttl_check = Date.now() - ttl
+  cache.forEach((val, i) => {
+    if ( val.t < ttl_check ) {
+      delete cache[i]
+    }
+  })
+}
